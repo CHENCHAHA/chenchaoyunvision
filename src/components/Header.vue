c7ad8d2c3f4caf94f97d43fd9d91def4d1e6c100
unknown <2499443165@qq.com>
2022-04-14 20:20:57 +0800

    完成了Header头部的制作

---
+++ src/components/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <!-- 头部导航栏区域 -->
+    <div class="content">
+      <!-- logo图标区域 -->
+      <div class="logo">
+        <router-link to="/index">
+          <img
+            src="../assets/logo/Visionlogo-bai.png"
+            title="Vision·CHENCHAOYUN"
+            alt=""
+          />
+        </router-link>
+      </div>
+      <!-- 导航分类区域 -->
+      <div class="nav">
+        <ul>
+          <router-link to="/index"><li>图片</li></router-link>
+          <router-link to="/index"><li>视频</li></router-link>
+          <router-link to="/index"><li>上传</li></router-link>
+          <router-link to="/index"><li>注册</li></router-link>
+          <router-link to="/index"
+            ><li><button>登录</button></li></router-link
+          >
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.content {
+  padding: 0 50px;
+  display: flex;
+  height: 80px;
+  background-color: black;
+  .logo {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    img {
+      cursor: pointer;
+      width: 100px;
+      height: 60px;
+    }
+  }
+  .nav {
+    flex: 3;
+    ul {
+      line-height: 80px;
+      display: flex;
+      justify-content: flex-end;
+      li {
+        font-size: 14px;
+        margin: 0 20px;
+        color: #fff;
+        letter-spacing: 2px;
+        button {
+          cursor: pointer;
+          height: 30px;
+          border-radius: 10px;
+          padding: 0 20px;
+        }
+        button:hover {
+          color: #fff;
+          background-color: aqua;
+          transition: all 0.5s;
+        }
+      }
+      li:hover {
+        color: aqua;
+        transition: all 0.2s;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="firstScreen">
+      <Header></Header>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.firstScreen {
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(235, 205, 205);
+}
+</style>
